b071821202a070b711d87a0769affab3e2e97e0b
xAkeno <clarkkentraguhos@gmail.com>
2025-08-10 21:53:44 +0800

    remove test

--- src/test/java/com/example/friendo/FriendoApplicationTests.java
+++ src/test/java/com/example/friendo/FriendoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.friendo;
+// package com.example.friendo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FriendoApplicationTests {
+// @SpringBootTest
+// class FriendoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
