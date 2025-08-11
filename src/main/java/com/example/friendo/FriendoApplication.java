59e7b05edf127dd70c64ecedc23ba458854b3318
xAkeno <clarkkentraguhos@gmail.com>
2025-08-11 19:31:48 +0800

    fix forget to add enablelschedule

--- src/main/java/com/example/friendo/FriendoApplication.java
+++ src/main/java/com/example/friendo/FriendoApplication.java
@@ -4,7 +4,9 @@ import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FriendoApplication {
 	private static Logger log = Logger.getLogger(FriendoApplication.class.getName());
