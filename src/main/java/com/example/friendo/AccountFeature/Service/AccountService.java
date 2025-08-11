4328b1368997a7320572a6eb28e456c08b3bb08b
xAkeno <clarkkentraguhos@gmail.com>
2025-08-11 10:15:57 +0800

    -fix 2

--- src/main/java/com/example/friendo/AccountFeature/Service/AccountService.java
+++ src/main/java/com/example/friendo/AccountFeature/Service/AccountService.java
@@ -266,7 +266,7 @@ public class AccountService {
                 for(Object[] imgRow : loadedImage){
                     ImageMetaModel image = new ImageMetaModel();
                     image.setId((Integer)imgRow[0]);
-                    image.setImageUrl((String)imgRow[2]);
+                    image.setImageUrl((String)imgRow[1]);
                     imageList.add(image);
                 }
                 feedDTO.setImageMetaModels(imageList);
